89ab67d42b8a761708a91c188d0915c5651c914a
Sachin Singh <30700344+sachinsinghsk13@users.noreply.github.com>
2021-06-10 16:38:04 +0530

    update by sachin added console.log

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const employeesroute=require("./routers/employees-router");
 const app=express();
 app.use(express.json());
 app.use("/employees",employeesroute);
+console.log("Update By Sachin");
 app.listen(8877,()=>{
     console.log("nodejs is connected successfully")
 }
